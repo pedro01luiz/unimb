5ebcd9f51dc9bcb2ee7c925267f73f830d3049c1
Pedro <Pedro@DESKTOP-MF33K9L>
2020-04-08 21:59:36 -0300

    08/04	21:59

---
+++ src/java/controller/UsuarioMBean.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import br.com.sige.entidade.Usuario;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+/**
+ *
+ * @author wender
+ */
+@Named(value = "usuarioMBean")
+@SessionScoped
+public class UsuarioMBean implements Serializable {
+
+    private Usuario usuario;
+    private List<Usuario> listaUsuario;
+    
+    private String valorPesquisar;
+    
+    public UsuarioMBean() {
+        
+    }
+    
+    @PostConstruct
+    public void init() {
+        this.usuario = new Usuario();
+        this.listaUsuario = new ArrayList<>();
+    }
+    
+    public String botaoNovo() {
+        this.usuario = new Usuario();
+        return "home?faces-redirect=true";
+    }
+    
+    public String botaoSalvar() {
+        listaUsuario.add(usuario);                
+        usuario = new Usuario();
+        return "consUsuario?faces-redirect=true";
+    }
+    
+    public void botaoPesquisar() {
+        
+    }
+    
+    public void botaoExcluir() {
+        
+    }
+    
+    public void botaoEditar() {
+        
+    }
+        
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<Usuario> getListaUsuario() {
+        return listaUsuario;
+    }   
+
+    public String getValorPesquisar() {
+        return valorPesquisar;
+    }
+
+    public void setValorPesquisar(String valorPesquisar) {
+        this.valorPesquisar = valorPesquisar;
+    }
+    
+    
+}
